aaad979b61278c740b8907ba88bd072dddb02a6e
mikityuk <konstantin.mikityuk@psi.ch>
2021-10-11 19:57:47 +0200

    javascript for processing jsons from data-management

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,26 @@
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+//
+// Read json file from ARMSTRONG database and pass it to function 
+//
+function read_json(json)
+{
+   fetch('https://raw.githubusercontent.com/armstrong-dev/data-management/main/json/' + json + '.json')
+  .then(response => response.json())
+  .then(obj => render_json(obj,json));
+}
+
+function render_json(obj,json)
+{
+   var d = document.getElementById('description')
+   d.innerHTML = '<b>download:</b><br>' + '<a href=https://github.com/armstrong-dev/data-management/raw/main/' + obj.extension + '/' + json + '.' + obj.extension + '>' + json + '.' + obj.extension + '</a><br><br>';
+   for(var key of Object.keys(obj))
+   {
+      d.innerHTML += '<hr>';
+      d.innerHTML += '<b>' + key + ':</b><br>' + obj[key] + '<br><br>';
+   }
+   
+   console.log(obj)
+}
--- rooster-e-spx1.i
+++ rooster-e-spx1.i
@@ -5,8 +5,8 @@
                         <div class="col-lg-8 col-xl-7 col-xxl-6">
                             <div class="breadcrumb"><a href="index.html">ARMSTRONG</a>&nbsp;:&nbsp;<a href="index.html#rooster">ROOSTER</a>&nbsp;:&nbsp;<a href="rooster-e.html">Examples</a>&nbsp;:&nbsp;SPX neutronics<div>
                             <div class="my-5 text-xl-start">
-                                <h1 class="display-5 fw-bolder text-dark mb-2 mt-4">SPX start-up test: static neutronics</h1>
-                                <p>Report: <a href=https://github.com/armstrong-dev/data-management/raw/main/c2ad8d16fa5d4361aa092b6bf734c560.pdf>c2ad8d16fa5d4361aa092b6bf734c560.pdf</a></p>
+                                <p id='description'></p>
+                                <script>read_json('c2ad8d16fa5d4361aa092b6bf734c560','pdf');</script>
                             </div>
                         </div>
                     </div>
