0160ce1332cad929463960ae3616c117d3b051d1
mikityuk <konstantin.mikityuk@psi.ch>
2021-10-12 18:28:08 +0200

    db-record.html generalized

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,49 @@
 */
 
 //
-// Read json file from ARMSTRONG database and pass it to function 
+// Read json file from ARMSTRONG database and pass it to function render
 //
-function read_json(json)
+function db_record(json)
 {
    fetch('https://raw.githubusercontent.com/armstrong-dev/data-management/main/json/' + json + '.json')
   .then(response => response.json())
-  .then(obj => render_json(obj,json));
+  .then(obj => render_record(obj,json));
 }
 
-function render_json(obj,json)
+function render_record(obj,json)
 {
    var d = document.getElementById('description')
-   d.innerHTML = '<b>download:</b><br>' + '<a href=https://github.com/armstrong-dev/data-management/raw/main/' + obj.extension + '/' + json + '.' + obj.extension + '>' + json + '.' + obj.extension + '</a><br><br>';
-   for(var key of Object.keys(obj))
+   d.innerHTML = '<div><b>download:</b></div>';
+   d.innerHTML += '<div><a href=https://github.com/armstrong-dev/data-management/raw/main/' + obj.extension + '/' + json + '.' + obj.extension + '>' + json + '.' + obj.extension + '</a></div><br>';
+   for(var key in obj)
    {
-      d.innerHTML += '<hr>';
-      d.innerHTML += '<b>' + key + ':</b><br>' + obj[key] + '<br><br>';
+      d.innerHTML += '<div><b>' + key + ':</b></div>';
+
+      if(Array.isArray(obj[key]))
+      {
+         // when array, assume that each element is an object
+         for(const e of obj[key])
+         {
+            for(var key2 in e)
+            {
+               d.innerHTML += '<div class="offset-1"><b>' + key2 + ':</b></div>';
+               try 
+               {
+                  var url = new URL(e[key2]);
+                  d.innerHTML += '<div class="offset-1"><a href=' + e[key2] + '>' + e[key2] + '</a></div>';
+               } 
+               catch (_) 
+               {
+                  d.innerHTML += '<div class="offset-1">' + e[key2] + '</div>';
+               }
+            }
+            d.innerHTML += '</br>';
+         }
+      }
+      else
+      {
+         d.innerHTML += '<div>' + obj[key] + '</div><br>';
+      }
    }
    
    console.log(obj)
